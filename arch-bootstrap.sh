f7847f17e1cca04cbc184bb6705f3a3fe9d7836c
John Stephenson <johnnyfleet@gmail.com>
2021-05-26 11:52:19 +1200

    minor typo update

--- arch-bootstrap.sh
+++ arch-bootstrap.sh
@@ -9,7 +9,7 @@ echo partition drive
 # we're going to simulate the manual input to fdisk
 # The sed script strips off all the comments so that we can 
 # document what we're doing in-line with the actual commands
-# Note that a blank line (commented as "defualt" will send a empty
+# Note that a blank line (commented as "default" will send a empty
 # line terminated with a newline to take the fdisk default.
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${BASE_DISK}
 	o # clear the in memory partition table
